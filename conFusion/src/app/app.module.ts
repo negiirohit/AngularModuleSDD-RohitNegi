8b52c01a0445ab2285fe8fe11ca71822470bfb1c
Rohit Negi <rohitnegi@smartdatainc.net>
2019-02-28 16:39:35 +0530

    Angular Routers Basics

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +29,8 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
