6d8585dcf570391851940316a83d7a8b3c4b58d3
Rohit Negi <rohitnegi@smartdatainc.net>
2019-03-01 11:01:00 +0530

    Angural Routing Parameter

--- conFusion/src/app/dish-detail/dish-detail.component.ts
+++ conFusion/src/app/dish-detail/dish-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common'; 
 import { Dish } from '../shared/dish';
-
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -10,13 +12,20 @@ import { Dish } from '../shared/dish';
 })
 export class DishDetailComponent implements OnInit {
 
-  @Input()
-  dish = Dish;
+  //@Input()
+  dish : Dish;
   
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
