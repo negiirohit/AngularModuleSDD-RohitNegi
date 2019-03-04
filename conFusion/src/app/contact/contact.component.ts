4a0b135adf46d39562d8102a8a14ff94a537bded
Rohit Negi <rohitnegi@smartdatainc.net>
2019-03-04 11:08:25 +0530

    reactive forms 2

--- conFusion/src/app/contact/contact.component.ts
+++ conFusion/src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
@@ -18,6 +18,9 @@ export class ContactComponent implements OnInit {
     feedback: Feedback;
     contactType = ContactType;
   
+    @ViewChild('fform') feedbackFormDirective;
+    
+
     constructor(private fb: FormBuilder) {
       this.createForm();
     }
@@ -27,20 +30,29 @@ export class ContactComponent implements OnInit {
   
     createForm() {
       this.feedbackForm = this.fb.group({
-        firstname: '',
-        lastname: '',
-        telnum: 0,
-        email: '',
+        firstname: ['', Validators.required ],
+        lastname: ['', Validators.required ],
+        telnum: ['', Validators.required ],
+        email: ['', Validators.required ],
         agree: false,
         contacttype: 'None',
         message: ''
       });
     }
-  
+
     onSubmit() {
       this.feedback = this.feedbackForm.value;
       console.log(this.feedback);
-      this.feedbackForm.reset();
+      this.feedbackForm.reset({
+        firstname: '',
+        lastname: '',
+        telnum: '',
+        email: '',
+        agree: false,
+        contacttype: 'None',
+        message: ''
+      });
+      this.feedbackFormDirective.resetForm();
     }
   
   }
